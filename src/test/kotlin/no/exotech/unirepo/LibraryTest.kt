51aaf7bdf8eac90cb73e28b464fb45b8f31e66ac
martinsw01 <martin.s.winther@gmail.com>
2020-10-31 20:48:21 +0100

    Test with junit

--- src/test/kotlin/no/exotech/unirepo/LibraryTest.kt
+++ src/test/kotlin/no/exotech/unirepo/LibraryTest.kt
@@ -3,8 +3,8 @@
  */
 package no.exotech.unirepo
 
-import kotlin.test.Test
-import kotlin.test.assertTrue
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 
 class LibraryTest {
     @Test fun testSomeLibraryMethod() {
