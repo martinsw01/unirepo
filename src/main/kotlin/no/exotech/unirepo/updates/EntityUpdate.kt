1877cc3b4fd752758cdfab21ac6dd10f5f3d398a
martinsw01 <martin.s.winther@gmail.com>
2020-12-08 22:16:45 +0100

    Delete unused data class

--- src/main/kotlin/no/exotech/unirepo/updates/EntityUpdate.kt
+++
@@ -1,7 +0,0 @@
-package no.exotech.unirepo.updates
-
-data class EntityUpdate(val column: String, val value: String) {
-    override fun toString(): String {
-        return "$column = $value"
-    }
-}
