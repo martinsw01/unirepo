39e2b6eb3e831e8c6648f4d9e3eb68e48e82fd8e
martinsw01 <martin.s.winther@gmail.com>
2020-12-08 22:32:14 +0100

    Created test for EntityUpdates.kt

---
+++ src/test/kotlin/no/exotech/unirepo/updates/EntityUpdatesTest.kt
@@ -0,0 +1,46 @@
+package no.exotech.unirepo.updates
+
+import no.exotech.unirepo.entities.BaseEntity
+import org.junit.jupiter.api.Assertions.assertIterableEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import javax.persistence.Transient
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class EntityUpdatesTest {
+
+    @Test
+    fun doesNotReturnNullFields() {
+        val entity = TestNullEntity(null, "string2")
+        val expectedColumns = listOf("str2")
+        val expectedValues = listOf("string2")
+        val (actualColumns, actualValues) = EntityUpdates.of(entity)
+        assertIterableEquals(expectedColumns, actualColumns)
+        assertIterableEquals(expectedValues, actualValues)
+    }
+
+    @Test
+    fun doesNotReturnTransientFields() {
+        val entity = TestTransientEntity("transient", "string2")
+        val expectedColumns = listOf("str2")
+        val expectedValues = listOf("string2")
+        val (actualColumns, actualValues) = EntityUpdates.of(entity)
+        assertIterableEquals(expectedColumns, actualColumns)
+        assertIterableEquals(expectedValues, actualValues)
+    }
+
+    @Test
+    fun worksWithGrandChildren() {
+        val entity = TestThirdGenEntity("transient", "string2", "string3")
+        val expectedColumns = listOf("str2", "str3")
+        val expectedValues = listOf("string2", "string3")
+        val (actualColumns, actualValues) = EntityUpdates.of(entity)
+        assertIterableEquals(expectedColumns, actualColumns)
+        assertIterableEquals(expectedValues, actualValues)
+    }
+
+
+    class TestNullEntity(val nullStr: String?, val str2: String?) : BaseEntity()
+    open class TestTransientEntity(@Transient val transientStr: String?, val str2: String?) : BaseEntity()
+    class TestThirdGenEntity(str1: String, str2: String, val str3: String) : TestTransientEntity(str1, str2)
+}
