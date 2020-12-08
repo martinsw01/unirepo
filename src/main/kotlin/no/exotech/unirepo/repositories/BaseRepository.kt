8d5092e5efc02ebfa1251f134a87c377ebf74677
martinsw01 <martin.s.winther@gmail.com>
2020-12-08 22:41:25 +0100

    Created a base repository with methods safely closing db connections.

---
+++ src/main/kotlin/no/exotech/unirepo/repositories/BaseRepository.kt
@@ -0,0 +1,40 @@
+package no.exotech.unirepo.repositories
+
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+
+open class BaseRepository(className: String, url: String, user: String, psw: String) {
+    protected val connection: Connection
+    init {
+        Class.forName(className)
+        connection = DriverManager.getConnection(url, user, psw)
+    }
+
+    fun <T> actOnPreparedStatement(prepStm: PreparedStatement, callback: (prepStm: PreparedStatement) -> T) : T {
+        val result: T
+        try {
+            result = callback(prepStm)
+        }
+        catch (e: Exception) {
+            prepStm.close()
+            throw e
+        }
+        prepStm.close()
+        return result
+    }
+
+    fun <T> actOnResultSet(rs: ResultSet, callback: (rs: ResultSet) -> T) : T {
+        val result: T
+        try {
+            result = callback(rs)
+        }
+        catch (e: Exception) {
+            rs.close()
+            throw e
+        }
+        rs.close()
+        return result
+    }
+}
